7e33da3ac755937b2b19a1ab17ad4a6f62de60b9
Nguyen Duong <tanduong03211@gmail.com>
2026-08-11 21:45:00 +0700

    update done hoàn chỉnh fix all

--- CodeTemp/template/DaThanThoai.java
+++
@@ -1,8 +0,0 @@
-package template;
-/**
- *
- * @author Truongbk
- */
-public class DaThanThoai {
-    public static byte[] data_shop;
-}
--- CodeTemp/template/Top_Dame.java
+++
@@ -1,9 +0,0 @@
-package template;
-/**
- *
- * @author Truongbk
- */
-public class Top_Dame {
-    public String name;
-    public long dame;
-}
